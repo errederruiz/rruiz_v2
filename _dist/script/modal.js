e4f5b480edab236df14464f6725e9b955f626e6a
rualmx <rualmx@gmail.com>
2018-08-25 14:49:45 -0500

    interactive links ready, just need to add some gif

--- _dist/script/modal.js
+++ _dist/script/modal.js
@@ -4,22 +4,11 @@ $(document).ready(function(){
         $(".container").toggleClass("noScroll");
         $(".invert").toggleClass("invertClickMe");
     });
+
+    $(".trigger li a").hover(function(){
+      var imgLink = $(this).attr('data-imgLink');
+      $('#' + imgLink).toggle();
+
+  });
+
 });
-
-const triggers = document.querySelectorAll('.trigger > li');
-const background  = document.querySelector('.popUp');
-const nav  = document.querySelector('.overlay');
-
-function handleEnter() {
-  this.classList.add('trigger-enter');
-  this.classList.add('trigger-enter-active');
-  background.classList.add('open');
-}
-
-function handleLeave() {
-  this.classList.remove('trigger-enter', 'trigger-enter-active');
-  background.classList.remove('open');
-}
-
-triggers.forEach(trigger => trigger.addEventListener('mouseenter', handleEnter));
-triggers.forEach(trigger => trigger.addEventListener('mouseleave', handleLeave));
